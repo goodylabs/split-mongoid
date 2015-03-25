6de210a5499370d7999bde160ea107d8515bc9cb
Paweł Lipka <lipkap@goodylabs.com>
2015-03-25 11:51:47 +0100

    bump split version to 1.2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require_relative "../lib/split-mongoid"
 require 'mongoid'
 require 'split'
 
-Mongoid::Config.sessions = {default: {uri: ENV['MONGOID_URL'] || "mongodb://127.0.0.1:27017/split-mongoid", options: {safe: true}}}
+Mongoid::Config.sessions = {default: {uri: ENV['MONGOID_URL'] || "mongodb://128.0.0.1:27017/split-mongoid", options: {}}}
 
 class User
   include Mongoid::Document
